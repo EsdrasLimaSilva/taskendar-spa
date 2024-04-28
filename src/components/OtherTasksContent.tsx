edd02fb488e758d517b13a339953c12dce39236e
Esdras Silva <esdraslimasilva83@gmail.com>
2024-04-28 16:03:52 -0300

    Add more tests and some test(api) utils

--- src/components/OtherTasksContent.tsx
+++ src/components/OtherTasksContent.tsx
@@ -10,7 +10,7 @@ interface VisualItemProps {
 
 const VisualCardItem = ({ unitValue, tasks }: VisualItemProps) => {
     return (
-        <article className="grid h-full grid-cols-[64px_1fr] items-center gap-2 bg-neutral-50">
+        <article className="visual-card-item grid h-full grid-cols-[64px_1fr] items-center gap-2 bg-neutral-50">
             <h3 className="flex h-full w-full min-w-[64px] items-center justify-center bg-neutral-400 text-center text-xl font-bold text-neutral-50">
                 {unitValue}
             </h3>
@@ -30,7 +30,7 @@ const VisualCardItem = ({ unitValue, tasks }: VisualItemProps) => {
     );
 };
 
-export default function AllTasksVisualContent() {
+export default function OtherTasksContent() {
     const { visual, taskList, loadingTasks } = useAppSelector(selectTasks);
 
     const visualResponse = getVisualContainers(
@@ -43,7 +43,10 @@ export default function AllTasksVisualContent() {
     if (loadingTasks.others) return <TaskLoadingIndicator />;
 
     return (
-        <section className="flex w-full flex-col gap-8 py-16">
+        <section
+            data-testid="other-tasks-content"
+            className="flex w-full flex-col gap-8 py-16"
+        >
             <h2 className="text-2xl text-neutral-600">
                 {visualResponse.modeLabel}
             </h2>
