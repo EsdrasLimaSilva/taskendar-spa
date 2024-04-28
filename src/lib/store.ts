adab40adb698a697c2073686253b786571cc3724
Esdras Silva <esdraslimasilva83@gmail.com>
2024-04-28 15:22:52 -0300

    Add more tests and some test utils

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,15 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 import tasksReducer from "./features/tasks/tasksSlice";
 
-export const makeStore = () => {
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+});
+
+export const makeStore = (preloadedState?: Partial<RootState>) => {
     return configureStore({
-        reducer: {
-            tasks: tasksReducer,
-        },
+        reducer: rootReducer,
+        preloadedState,
     });
 };
 
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppStore = ReturnType<typeof makeStore>;
-export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
