579c1f51a3581450ee9798565dd57d9e91f0e9d5
Esdras Silva <esdraslimasilva83@gmail.com>
2024-04-28 08:42:16 -0300

    Fix some bugs

--- src/components/AdminEditTaskCard.tsx
+++ src/components/AdminEditTaskCard.tsx
@@ -16,6 +16,7 @@ export default function AdminEditTaskCard({ task }: { task: TaskType }) {
     const { getAccessTokenSilently } = useAuth0();
     const [deleteConfirmationVisible, setDeleteConfirmationVisible] =
         useState(false);
+
     const dispatch = useAppDispatch();
 
     const showConfirmation = () => {
@@ -36,7 +37,7 @@ export default function AdminEditTaskCard({ task }: { task: TaskType }) {
         <li className=" relative flex item-center justify-between text-xl bg-neutral-50 p-4 text-neutral-600 rounded-md overflow-clip">
             <span
                 className={`flex flex-row justify-around items-center bg-neutral-100 border-2 border-neutral-200 absolute top-0 right-0 bottom-0 text-2xl gap-2 [&>button]:text-3xl w-[12%] min-w-[80px] translate-x-full transition-all ${
-                    deleteConfirmationVisible && "translate-x-0"
+                    deleteConfirmationVisible && "-translate-x-0"
                 }`}
             >
                 <button type="button" onClick={deleteTask}>
@@ -48,20 +49,20 @@ export default function AdminEditTaskCard({ task }: { task: TaskType }) {
             </span>
             <h4>{task.title}</h4>
 
-            <button
-                className="flex flex-row item-center"
-                type="button"
-                onClick={showConfirmation}
-            >
-                <IoMdTrash className="text-3xl" />
-                <MdEdit
-                    className="text-3xl"
-                    onClick={() => {
-                        dispatch(setTargetEditTask(task._id));
-                        dispatch(setEditModalVisible());
-                    }}
-                />
-            </button>
+            <div className="flex flex-row item-center">
+                <button type="button" onClick={showConfirmation}>
+                    <IoMdTrash className="text-3xl" />
+                </button>
+                <button>
+                    <MdEdit
+                        className="text-3xl"
+                        onClick={() => {
+                            dispatch(setTargetEditTask(task._id));
+                            dispatch(setEditModalVisible());
+                        }}
+                    />
+                </button>
+            </div>
         </li>
     );
 }
